54431c55603b6597403fa77c3616cb789dc62a5d
Rodrigo Carvalho <Rodrigogt1994@hotmail.com>
2022-05-12 15:41:54 -0300

    teste

---
+++ src/com/dio/Teste.java
@@ -0,0 +1,25 @@
+package com.dio;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		int i;
+		//int i;
+		int I;
+		//int 1a;
+		int _1a;
+		int $aq;
+		
+		i = 5;
+		I = 10;
+		_1a = 20;
+		$aq = 7;
+		
+		final int j = 10;
+		//j = 15;
+		
+		System.out.println(i);
+
+	}
+
+}
